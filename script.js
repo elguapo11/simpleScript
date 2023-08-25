7a616825df0333c014d0a0a1ffc38f9596baa8c4
Benny Mendoza <bennybc9387@gmail.com>
2023-08-25 09:53:58 -0600

    updated so it goes double

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 // Define the possible values
-const possibleValues = ['hidden', 'visible', 'replace', 'autoreplace'];
+const possibleValues = ['"hidden"', '"visible"', '"replace"', '"autoreplace"'];
 
 // Function to generate a random item
 function generateRandomItem() {
-  const key = Math.floor(100000 + Math.random() * 900000); // Generate a 6-digit key
+  const key = `"${Math.floor(100000 + Math.random() * 900000)}"`; // Generate a 6-digit key with double quotes
   const value =
-    possibleValues[Math.floor(Math.random() * possibleValues.length)]; // Select a random value
-  return { [key.toString()]: value }; // Return as a key-value pair in an object
+    possibleValues[Math.floor(Math.random() * possibleValues.length)]; // Select a random value with double quotes
+  return `${key}: ${value}`; // Return as a key-value pair in a string
 }
 
 // Function to generate a list of items as a single object
 function generateItemObject(count) {
-  const itemObject = {};
+  const items = [];
   for (let i = 0; i < count; i++) {
-    const item = generateRandomItem();
-    Object.assign(itemObject, item); // Merge each item into the main object
+    items.push(generateRandomItem());
   }
-
-  return itemObject;
+  return `{ ${items.join(', ')} }`; // Return as a single object string
 }
 
 // Generate a list of 1000 items (you can change the count as desired)
 const itemCount = 1000;
-const itemObject = generateItemObject(itemCount);
+const itemObjectString = generateItemObject(itemCount);
 
-// Output the generated object to the console
-console.log(itemObject);
+// Output the generated object string to the console
+console.log(itemObjectString);
