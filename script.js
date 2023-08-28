3851b524d0dd122f33ac40912538582b7f2ad76d
Benny Mendoza <bennybc9387@gmail.com>
2023-08-28 17:26:08 -0600

    saving changes

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 const possibleValues = ['"hidden"', '"visible"', '"replace"', '"autoreplace"'];
 
 // Function to generate a random item
-function generateRandomItem() {
-  const key = `"${Math.floor(100000 + Math.random() * 900000)}"`; // Generate a 6-digit key with double quotes
+function generateRandomItem(key, maxValue) {
+  if (key > maxValue) {
+    return null;
+  }
   const value =
     possibleValues[Math.floor(Math.random() * possibleValues.length)]; // Select a random value with double quotes
   return `${key}: ${value}`; // Return as a key-value pair in a string
@@ -12,14 +14,14 @@ function generateRandomItem() {
 // Function to generate a list of items as a single object
 function generateItemObject(count) {
   const items = [];
-  for (let i = 0; i < count; i++) {
-    items.push(generateRandomItem());
+  for (let i = 1; i < count; i++) {
+    items.push(generateRandomItem(i));
   }
   return `{ \n  ${items.join(',\n  ')} \n}`; // Return as a single object string with indentation
 }
 
 // Generate a list of 1000 items (you can change the count as desired)
-const itemCount = 1000;
+const itemCount = 50;
 const itemObjectString = generateItemObject(itemCount);
 
 // Output the generated object string to the console with formatting
